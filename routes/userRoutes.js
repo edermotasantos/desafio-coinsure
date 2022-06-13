c3125a51b4df5def6de53882a1d668b70a08d74e
Eder Santos <3derms@gmail.com>
2022-06-13 18:26:06 -0300

    refactor: separa front end e back end para cada pasta

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const User = require('../controllers/user');
-
-router.post('/', User.createUser);
-router.get('/', User.listAllUsers);
-router.get('/:id', User.listUserById);
-
-module.exports = router;
