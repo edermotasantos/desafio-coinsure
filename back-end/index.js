1f98c46a42c35889badf23b393bfe440e4ad46b5
Eder Santos <3derms@gmail.com>
2022-07-16 22:42:14 -0300

    fix: corrige port

--- back-end/index.js
+++ back-end/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const cors = require('cors');
 
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
